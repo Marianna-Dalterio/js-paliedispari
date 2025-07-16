baf1d4c61c649486aa9625580ec38072d0f476ed
Marianna-Dalterio <mariannadalterio00@gmail.com>
2025-07-16 12:23:56 +0200

    first commit

---
+++ palindroma/main.js
@@ -0,0 +1,23 @@
+//Chiedere all’utente di inserire una parola
+let parolaUtente = prompt("Inserisci una parola")
+//Creare una funzione per capire se la parola inserita è palindroma
+
+//ciclo for per analizzare caratteri di una parola da sx-dx e viceversa
+let parolaSxDx = []
+for (let i = 0; i < parolaUtente.length; i++) {
+    caratteriSxDx = parolaUtente[i]
+    console.log(caratteriSxDx)
+}
+let parolaDxSx = []
+for (let i = parolaUtente.length - 1; i >= 0; i--) {
+    caratteriDxSx = parolaUtente[i]
+    console.log(caratteriDxSx)
+}
+
+//condizione per confronto
+let parolaPalindroma = caratteriSxDx === caratteriDxSx
+if (parolaPalindroma) {
+    console.log("La tua parola E' palindroma")
+} else {
+    console.log("La tua parola NON è palindroma")
+}
